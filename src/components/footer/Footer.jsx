cf2827207dd178e57cd141dc2e3c2b0183ae0a3f
VladPozdnyakov <vladpozcrypto@gmail.com>
2024-10-21 23:49:02 +0200

    Edits part 1

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom"; // Импортируем Link из react-router-dom
 import { useTranslation } from "react-i18next"; // Импортируем useTranslation
 import styles from "./footer.module.scss";
 import Logotype from "../../assets/images/Logotype.png";
 
 const Footer = ({ transparent, color }) => {
+  const [language, setLanguage] = useState("en");
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
@@ -13,7 +14,8 @@ const Footer = ({ transparent, color }) => {
   }, [i18n.language]);
 
   const switchLanguage = (lng) => {
-    i18n.changeLanguage(lng);
+    setLanguage(lng);
+    i18n.changeLanguage(lng); // Переключение языка с помощью i18n
   };
 
   return (
@@ -66,14 +68,28 @@ const Footer = ({ transparent, color }) => {
             </li>
           </ul>
         </nav>
-        <div className={styles.button}>
-          <span className={styles.buttonText}>{t("request_demo")}</span>
+        <div style={{ flex: "1.5" }}>
+          <div className={styles.button}>
+            <span className={styles.buttonText}>{t("request_demo")}</span>
+          </div>
         </div>
         <div>
-          {/* Переключатель языков */}
-          <button onClick={() => switchLanguage("en")}>English</button>
-          <button onClick={() => switchLanguage("heb")}>עברית</button>
-          {/* Исправлено на "heb" */}
+          <select
+            value={language}
+            onChange={(e) => switchLanguage(e.target.value)}
+            style={{
+              border: "none",
+              background: "none", // Убираем фон
+              fontSize: "16px", // Размер шрифта
+              outline: "none", // Убираем активный бордер
+              textAlignLast: "right", // Выравниваем текст и стрелку справа
+              color: "#363B61", // Цвет текста (белый для темного фона)
+              flex: "1.5",
+            }}
+          >
+            <option value="en">EN</option>
+            <option value="heb">בקש הדגמה</option>
+          </select>
         </div>
       </div>
     </div>
